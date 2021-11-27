972c9507a085c60d185a29ead107734e3a7ecae7
samuba <samuel.z.bach@gmail.com>
2021-11-27 14:35:49 +0100

    fix generating new link on browser back

--- src/routes/created.svelte
+++ src/routes/created.svelte
@@ -1,10 +1,18 @@
 <script context="module" lang="ts">
 	import { getHost } from '$lib/host';
 	import type { Link } from 'src/global';
+	import { onMount } from 'svelte';
 
 	/** @type {import('@sveltejs/kit').Load} */
 	export async function load({ page, fetch }) {
 		console.log({ page });
+		const shortUrl = page.query.get('shortUrl');
+		if (shortUrl) {
+			return {
+				status: 200,
+				props: { shortUrl }
+			};
+		}
 		const url = page.query.get('url');
 		const description = page.query.get('description');
 
@@ -25,8 +33,18 @@
 </script>
 
 <script lang="ts">
+	import { browser } from '$app/env';
 	import CreateLink from './_createLink.svelte';
 	export let shortUrl: string;
+
+	onMount(() => {
+		if (browser) {
+			// to not generate a new link when users clicks on browser back
+			if (!location.search.includes('?shortUrl')) {
+				location.search = '?shortUrl=' + shortUrl;
+			}
+		}
+	});
 </script>
 
 <CreateLink {shortUrl} />
