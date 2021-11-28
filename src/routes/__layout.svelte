e75abdab95cf5bc50c07a4d59bf25eb3fd5d8aa0
samuba <samuel.z.bach@gmail.com>
2021-11-28 15:00:31 +0100

    proper favicon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,18 @@
 <script>
 	import CocktailIcon from '$lib/IconCocktail.svelte';
+
+	const title = 'Samu.Bar';
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
 <center>
 	<header>
 		<h1>
 			<CocktailIcon style="height: 1.8rem;" color="#3291ff" />
-			Samu.Bar
+			{title}
 		</h1>
 		<p>Short URLs served chilled</p>
 	</header>
